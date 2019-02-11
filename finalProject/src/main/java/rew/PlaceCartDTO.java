d62827d39e1cb6aebd964056f31b9ec8b5d50d30
user <user@DESKTOP-7EQF3M8>
2019-02-11 09:31:22 +0900

    2019-02-11 09:30 유주빈

--- finalProject/src/main/java/rew/PlaceCartDTO.java
+++ finalProject/src/main/java/rew/PlaceCartDTO.java
@@ -1,36 +1,35 @@
 package rew;
 
 public class PlaceCartDTO {
-	
+
 	private String mid;
+	//회원 아이디;
 	private String pid;
+	//관광명소 아이디
+	private String title;
+	// 명소이름
+	private String latitude;
+	// 위도
+	private String longitude;
+	// 경도
+	private String mainImg;
+	// 메인이미지
 	
-	public String getMid() {
-		return mid;
-	}
-	public void setMid(String mid) {
-		this.mid = mid;
-	}
-	public String getPid() {
-		return pid;
-	}
-	public void setPid(String pid) {
-		this.pid = pid;
-	}
 	
-	public PlaceCartDTO(String mid, String pid) {
-		super();
-		this.mid = mid;
-		this.pid = pid;
-	}
 	
-	public PlaceCartDTO() {
-		// TODO Auto-generated constructor stub
-	}
 	
+	
+	
+	
+
 	@Override
 	public String toString() {
-		return "[mid : "+mid+"] /"+" [pid : "+pid+"]";
+		return "[mid : " + mid + "]" + "\n"+
+	           "[pid : " + pid + "]" + "\n"+
+	           "[title : " + title + "]" + "\n"+
+	           "[latitude : " + latitude + "]" + "\n"+
+	           "[longitude : " + longitude + "]" + "\n"+
+	           "[mainImg : " + mainImg + "]" + "\n";
 	}
 
 }

d5073a72d9d7d65c0a1694edc8337f9315933eda
user <user@DESKTOP-7EQF3M8>
2019-02-13 11:55:25 +0900

    sadsad

--- finalProject/src/main/java/com/itbank/springProject/kim/controller/MemberController.java
+++ finalProject/src/main/java/com/itbank/springProject/kim/controller/MemberController.java
@@ -7,18 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.itbank.springProject.kim.db.MemberDAO;
 
-@Controller
 public class MemberController {
 
 	@Autowired
 	@Qualifier("MemberDAO")
-	private MemberDAO memberDAO;
 
 	@RequestMapping("kim/selectAllMember")
 	public void memberSelectAll(Model model) {
-		model.addAttribute("list", memberDAO.selectAll());
 	}
 
 }

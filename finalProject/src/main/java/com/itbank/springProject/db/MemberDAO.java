53c5176cb69f1e01539ada8bb19a4489bed94154
hn2441 <hn2441@daum.net>
2019-02-18 16:44:33 +0900

    Signed-off-by: hn2441 <hn2441@daum.net>

--- finalProject/src/main/java/com/itbank/springProject/db/MemberDAO.java
+++ finalProject/src/main/java/com/itbank/springProject/db/MemberDAO.java
@@ -2,7 +2,7 @@ package com.itbank.springProject.db;
 
 import java.util.List;
 
-import org.mybatis.spring.SqlSessionTemplate;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +13,11 @@ public class MemberDAO{
 	//DAO == Member테이블 DB를 처리할 수 있는 클래스
 	
 	@Autowired
-	SqlSessionTemplate myBatis;
+	private SqlSession session;
 
 	public void insert(MemberDTO dto) {
 		try {
-			myBatis.insert("member.insert", dto);	//member : member.xml 파일의 namespace
+			session.insert("member.insert", dto);	//member : member.xml 파일의 namespace
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -25,22 +25,22 @@ public class MemberDAO{
 	
 	public void update(MemberDTO dto){
 		try {
-			myBatis.update("member.update", dto);
+			session.update("member.update", dto);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void delete(MemberDTO dto) throws Exception {
-		myBatis.delete("member.delete", dto);
+		session.delete("member.delete", dto);
 	}
 	
 	public MemberDTO select(MemberDTO dto) throws Exception {	
-		return myBatis.selectOne("member.select", dto);
+		return session.selectOne("member.select", dto);
 	}
 	
 	public List<MemberDTO> selectAll() throws Exception {
-		return myBatis.selectList("member.selectAll");
+		return session.selectList("member.selectAll");
 	}
 	
 }

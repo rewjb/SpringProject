5aa6d75492c1f977073835dc51927a6354091e55
user <user@DESKTOP-7EQF3M8>
2019-02-18 17:07:42 +0900

    조광재 17:07 2019-02-18

--- finalProject/src/main/java/com/itbank/springProject/db/AttractionsDAO.java
+++ finalProject/src/main/java/com/itbank/springProject/db/AttractionsDAO.java
@@ -1,15 +1,19 @@
 package com.itbank.springProject.db;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("attractionsDAO")
 public class AttractionsDAO {
 
-	public void select() {
-		
+	
+	@Autowired
+	private SqlSession session;
+	
+	
+	public AttractionsDTO select(String id) {//고유 아이디를 통한  select문
+		return session.selectOne("attractions.select", id);
 	}
-	
-	
-	
 	
 }
--- finalProject/src/main/java/com/itbank/springProject/joe/controller/J_CartController.java
+++ finalProject/src/main/java/com/itbank/springProject/joe/controller/J_CartController.java
@@ -3,6 +3,7 @@ package com.itbank.springProject.joe.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itbank.springProject.db.AttractionsDAO;
@@ -23,9 +24,9 @@ public class J_CartController {
 	
 	
 	@RequestMapping("cartInsert")
-	public String cartInsert(PlaceCartDTO placeCartDTO) {
+	public String cartInsert(PlaceCartDTO placeCartDTO ,Model model) {
 		placeCartDAO.cartInsert(placeCartDTO);
-		
+		model.addAttribute("dto", attractionsDAO.select(placeCartDTO.getPid()));
 		return "j_cart";
 	}
 	

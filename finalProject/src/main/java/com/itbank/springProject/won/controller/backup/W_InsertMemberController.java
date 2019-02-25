f55a90d713cb775ee0077822ceccf7e443fb8406
bjy0220 <bjy0220@gmail.com>
2019-02-25 10:04:03 +0900

    na

--- finalProject/src/main/java/com/itbank/springProject/won/controller/backup/W_InsertMemberController.java
+++ finalProject/src/main/java/com/itbank/springProject/won/controller/backup/W_InsertMemberController.java
@@ -1,4 +1,4 @@
-package com.itbank.springProject.won.controller;
+package com.itbank.springProject.won.controller.backup;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;

26626dda9311fa54ca0567d782631387adafb185
user <user@java>
2019-02-25 09:15:05 +0900

    조광재

--- finalProject/src/main/java/com/itbank/springProject/db/Mongo_ShareProjectDAO.java
+++ finalProject/src/main/java/com/itbank/springProject/db/Mongo_ShareProjectDAO.java
@@ -23,7 +23,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
-@Repository("Mongo_ShareProjectDAO")
 public class Mongo_ShareProjectDAO {
 
 	

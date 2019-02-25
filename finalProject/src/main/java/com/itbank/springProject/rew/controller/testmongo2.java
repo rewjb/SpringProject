d9f35c4bd1ec3bffb93e7bcece867c9360017706
user <user@user-PC>
2019-02-25 09:17:44 +0900

    AS

--- finalProject/src/main/java/com/itbank/springProject/rew/controller/testmongo2.java
+++ finalProject/src/main/java/com/itbank/springProject/rew/controller/testmongo2.java
@@ -25,22 +25,27 @@ public class testmongo2 {
 
 		MongoClientURI uri = new MongoClientURI(
 				"mongodb+srv://rew:1234@rewjubinmongo-g7aac.mongodb.net/test?retryWrites=true");
-
-		MongoClient mongoClient = new MongoClient("211.181.190.201", 27017);
-
 		
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "tag");
+		
 
-		MongoCollection<Document> collectionPlace = mongoTemplate.getCollection("place");
-		MongoCollection<Document> collectionsharePlace = mongoTemplate.getCollection("sharePlace");
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase data =  mongoClient.getDatabase("tag");
+		
+//		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "tag");
 
-		List<Document> condition = new ArrayList<>();
+		MongoCollection<Document> collectionPlace = data.getCollection("place");
+//		MongoCollection<Document> collectionsharePlace = data.getCollection("sharePlace");
 
-		condition.add(new Document("id", "1000000212101"));
-		condition.add(new Document("id", "1000000348101"));
-		condition.add(new Document("id", "1000000354101"));
+//		List<Document> condition = new ArrayList<>();
+//
+//		condition.add(new Document("id", "1000000212101"));
+//		condition.add(new Document("id", "1000000348101"));
+//		condition.add(new Document("id", "1000000354101"));
+		
+		collectionPlace.insertOne(new Document("dd","sad"));
 
-		MongoCursor<Document> result = collectionPlace.find(new Document("$or", condition)).iterator();
+		MongoCursor<Document> result = collectionPlace.find().iterator();
+//		MongoCursor<Document> result = collectionPlace.find(new Document("$or", condition)).iterator();
 
 		while (result.hasNext()) {
 			System.out.println(result.next().toJson());
